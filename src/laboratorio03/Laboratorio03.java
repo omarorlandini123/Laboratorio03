9b3b0fb484c8cb7d20d4d4594538f02107b0d3f0
Omar Orlandini <omarorlandini123@gmail.com>
2023-09-02 13:29:59 -0500

    Add files via upload

---
+++ src/laboratorio03/Laboratorio03.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio03;
+
+/**
+ *
+ * @author DOCENTE
+ */
+public class Laboratorio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        FrameListaPersona pantalla = 
+                new FrameListaPersona();
+        pantalla.setLocationRelativeTo(null);
+        pantalla.setVisible(true);
+        
+    }
+    
+}
---
+++ src/laboratorio03/Persona.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio03;
+
+/**
+ *
+ * @author DOCENTE
+ */
+public class Persona {
+    
+    private String nombre;
+    private String apellido;
+    private String dni;
+
+    public Persona(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String consola() {
+        return this.nombre + " " +
+                this.apellido + " | "+
+                this.dni;                        
+    }
+    
+    
+    
+}
